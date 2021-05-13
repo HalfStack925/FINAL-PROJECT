08f7a5b2eb37ded743b80905a29f9f66a47cc267
Mike Winchel <mw0183163@otc.edu>
2021-05-13 18:35:47 -0500

    Initial commit

--- app/src/main/java/com/example/weathernow/Conditions.java
+++ app/src/main/java/com/example/weathernow/Conditions.java
@@ -2,24 +2,49 @@ package com.example.weathernow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.number.FormattedNumberRange;
 import android.os.Bundle;
+import android.text.style.TtsSpan;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.time.Clock;
-import java.time.LocalTime;
 import java.util.Calendar;
+import java.util.Formatter;
+
 
 public class Conditions extends AppCompatActivity {
+    String degreeSymbol = "°",
+        apiKey = "4970dc8241ecfecae0efeeaaf46d7772",
+        degrees = "imperial",
+        city,
+        state;
+    RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conditions);
+        getAPI();
 
         //display current date and time
         displayDateandTime();
+
 
     }
 
@@ -36,6 +61,56 @@ public class Conditions extends AppCompatActivity {
 
         textViewDate.setText(currentDate);
         textViewTime.setText(currentTime);
+
+    }
+
+    public void getAPI(){
+        requestQueue = Volley.newRequestQueue(this);
+        city = getIntent().getStringExtra("city");
+        state = getIntent().getStringExtra("state");
+
+       // String url = "api.openweathermap.org/data/2.5/weather?q="+searchedCity+"&"+searchedSate+"&units="+ degrees +"&appid="+apiKey
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&"+state+"&units="+degrees+"&appid="+apiKey;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    DecimalFormat decimalFormat = new DecimalFormat("##");
+
+                    JSONObject object = response.getJSONObject("main");
+                    int currentTemp = object.getInt("temp");
+                    int currentHumidity = object.getInt("humidity");
+                    int currentLowTemp = object.getInt("temp_min");
+                    int currentHighTemp = object.getInt("temp_max");
+
+
+                    TextView txtCurrentTemp = findViewById(R.id.txtCurrentTemp);
+                    txtCurrentTemp.setText(decimalFormat.format(currentTemp)+degreeSymbol);
+
+                    TextView txtCurrentHumidity = findViewById(R.id.txtCurrentHumidity);
+                    txtCurrentHumidity.setText(decimalFormat.format(currentHumidity));
+
+                    TextView txtCurrentLowTemp = findViewById(R.id.txtCurrentLowTemp);
+                    txtCurrentLowTemp.setText(decimalFormat.format(currentLowTemp)+degreeSymbol);
+
+                    TextView txtCurrentHighTemp = findViewById(R.id.txtCurrentHighTemp);
+                    txtCurrentHighTemp.setText(decimalFormat.format(currentHighTemp)+degreeSymbol);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+
 
     }
 
--- app/src/main/java/com/example/weathernow/MainActivity.java
+++ app/src/main/java/com/example/weathernow/MainActivity.java
@@ -2,43 +2,63 @@ package com.example.weathernow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    String currentCity;
-    EditText searchCity;
-    Spinner searchState;
-    RadioButton rdCurrent;
-    Button btnSearch;
+
+   EditText searchCity;
+   Spinner searchState;
+   Button btnsearch;
+   String selectedCity, selectedState;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rdCurrent = findViewById(R.id.rdCurrent);
-        searchCity = findViewById(R.id.txtCitySearch);
-        searchState = findViewById(R.id.spnStates);
-        btnSearch = findViewById(R.id.btnSearch);
+
+       searchCity = findViewById(R.id.txtSearchCity);
+       searchState = findViewById(R.id.spnStates);
 
 
-        btnSearch.setOnClickListener(new View.OnClickListener() {
+       btnsearch = findViewById(R.id.btnSearch);
+
+        btnsearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Conditions.class);
+
+                Intent intent = new Intent(MainActivity.this,Conditions.class);
+                intent.putExtra("city",searchCity.getText().toString());
+                intent.putExtra("state",searchState.getSelectedItem().toString());
                 startActivity(intent);
-            }
+
+//
+          }
+
         });
 
-        
     }
-
-
-
 }
